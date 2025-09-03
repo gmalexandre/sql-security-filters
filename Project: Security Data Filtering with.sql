e506f42e07631023f5519e8dd914ab5e1d2e6bd9
Gabriel Alexandre <136840666+gmalexandre@users.noreply.github.com>
2025-09-03 20:30:18 -0300

    Update and rename -- Project: Security Data Filtering with.sql to Project: Security Data Filtering with.sql

--- Project: Security Data Filtering with.sql
+++ Project: Security Data Filtering with.sql
@@ -1,12 +1,11 @@
 -- Project: Security Data Filtering with SQL
--- Author: [Gabriel]
 -- Description: SQL queries to investigate suspicious login attempts and identify employee machines for security updates.
 
 -- 1. Retrieve failed login attempts after business hours (after 18:00)
 SELECT * 
 FROM log_in_attempts
 WHERE login_time > '18:00:00' 
-AND success = 0;
+  AND success = 0;
 
 -- 2. Retrieve login attempts on specific dates (2022-05-08 and 2022-05-09)
 SELECT * 
@@ -16,13 +15,14 @@ WHERE login_date IN ('2022-05-08', '2022-05-09');
 -- 3. Retrieve login attempts outside of Mexico
 SELECT * 
 FROM log_in_attempts
-WHERE country NOT LIKE 'MEX%' AND country NOT LIKE 'MEXICO';
+WHERE country NOT LIKE 'MEX%' 
+  AND country NOT LIKE 'MEXICO';
 
 -- 4. Retrieve employees in the Marketing department in the East building
 SELECT * 
 FROM employees
 WHERE department = 'Marketing' 
-AND office LIKE 'East-%';
+  AND office LIKE 'East-%';
 
 -- 5. Retrieve employees in Finance or Sales departments
 SELECT * 
